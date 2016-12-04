ef19c3590aeffce0f000cd6f1f6c96351f204913
Larry Natalicio <l.natalicio@icloud.com>
2016-12-03 22:07:28 -0500

    Animation 11 - Install SwiftGif library and display gif

--- Animation 11 - GifAnimation/GifAnimation/GifAnimation/ViewController.swift
+++ Animation 11 - GifAnimation/GifAnimation/GifAnimation/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftGifOrigin
 
 class ViewController: UIViewController {
     
@@ -14,18 +15,23 @@ class ViewController: UIViewController {
     
     struct Constants {
         struct Gifs {
-            static let catVideo = "cat-video.gif"
+            static let catVideo = "cat-video"
         }
     }
     
     // MARK: - Properties
 
+    @IBOutlet weak var gifImageView: UIImageView!
     
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        /*
+         Use third-party library `SwiftGif` to display gif.
+         */
+        gifImageView.loadGif(name: Constants.Gifs.catVideo)
     }
     
     // MARK: - Status Bar
